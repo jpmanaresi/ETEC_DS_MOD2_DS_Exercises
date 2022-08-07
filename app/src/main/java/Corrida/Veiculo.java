534224955486cfe1293a5c579e3f198debfdbb43
jpmanaresi <jpmanaresi@gmail.com>
2022-08-07 20:24:30 -0300

    .

---
+++ app/src/main/java/Corrida/Veiculo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Corrida;
+
+/**
+ *
+ * @author Admin
+ */
+public class Veiculo {
+    IMotor motor;
+    
+    Veiculo (IMotor m){
+        this.motor = m;
+    }
+    public String acelerar(){
+    return this.motor.acelerar();
+}
+}
